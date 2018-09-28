b89111ecd1521a5ede646ec04ee4aeffafad4311
Erin Tait <erinmtait@gmail.com>
2018-09-28 13:02:02 -0700

    completed 0.5 memory match

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
 var first_card_source='';
 var second_card_source='';
