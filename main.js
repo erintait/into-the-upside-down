c6930c68186fa77f558882ed4d38326d60a535ed
Erin Tait <erinmtait@gmail.com>
2019-01-07 09:22:58 -0800

    removed some files that were no longer needed

--- main.js
+++ main.js
@@ -10,29 +10,6 @@ var first_card_source='';
 var second_card_source='';
 var cant_click_card = false;
 
-//priorities--
-
-// more functionality:
-// dynamically create game board with jquery?
-
-// fix css and make it not so ugly
-// edit photos to at least give them a border so it looks better against the background of the page
-
-// potential things for later-
-// ask the owners of internet dogs i follow if i can use their dogs' photos - likely would only do this after the first set is finished and the css is fixed, so there is something to show them when i approach them.
-// perhaps when a match involving that dog is completed, there is a link offered to information about the breed and any related social media fan pages the owner may have for their dog
-// if this falls through - maybe try to find other professional stock photos with similar backgrounds - may look more professional if the backdrops are similar, would also make the dogs themselves stand out more
-// maybe play random dog-related sound effect when a match is made
-
-// known bugs to fix:
-// 1 when you click the same image twice, it sees the sources are the same and considers them a match
-// 2 if you click an unmatched card and then click a previously matched card, it sees they do not match, and flips both the unmatched AND the previously matched cards over.
-    // possible solution: put all the playable cards into an array, remove them from the array of clickable elements once it is matched?
-
-//setTimeout does not actually disable clicks apparently, need to pick something else
-//possibly temporarily use pointer events none?
-
-
 //stats variables
 var matches = 0;
 var attempts = 0;
@@ -50,6 +27,7 @@ function initializeApp(){
 function displayStats(){
     $(".games-played .value").text(games_played);
     $('.attempts .value').text(attempts);
+    console.log('attempts', attempts);
     var accuracyMath = matches/attempts * 100;
     accuracy = accuracyMath.toFixed(2) + '%';
     $('.accuracy .value').text(accuracy);
@@ -68,7 +46,26 @@ function reset_stats(){
         }
     displayStats();
     $('.card').removeClass('hide');
-    cardArray = ["dog-cardA.png", "dog-cardB.png", "dog-cardC.png", "dog-cardD.png", "dog-cardE.png", "dog-cardF.png", "dog-cardG.png", "dog-cardH.png", "dog-cardI.png", "dog-cardA.png", "dog-cardB.png", "dog-cardC.png", "dog-cardD.png", "dog-cardE.png", "dog-cardF.png", "dog-cardG.png", "dog-cardH.png", "dog-cardI.png"];
+    cardArray = [
+        "images/card01.png",
+        "images/card02.png",
+        "images/card03.png",
+        "images/card04.png",
+        "dog-cardE.png",
+        "dog-cardF.png",
+        "dog-cardG.png",
+        "dog-cardH.png",
+        "dog-cardI.png",
+        "dog-cardA.png",
+        "dog-cardB.png",
+        "dog-cardC.png",
+        "dog-cardD.png",
+        "dog-cardE.png",
+        "dog-cardF.png",
+        "dog-cardG.png",
+        "dog-cardH.png",
+        "dog-cardI.png"
+    ];
     appendRandomizedCards();
 }
 
@@ -115,18 +112,37 @@ function handleCardClick(){
             }
         }
         else {
-            cant_click_card=true;
+            cant_click_card = true;
             attempts++;
             displayStats();
             setTimeout(hideMismatchedCards, 2000);
-            cant_click_card=false;
+            cant_click_card = false;
             return
         }
     }
 
 }
 
-var cardArray = ["dog-cardA.png", "dog-cardB.png", "dog-cardC.png", "dog-cardD.png", "dog-cardE.png", "dog-cardF.png", "dog-cardG.png", "dog-cardH.png", "dog-cardI.png", "dog-cardA.png", "dog-cardB.png", "dog-cardC.png", "dog-cardD.png", "dog-cardE.png", "dog-cardF.png", "dog-cardG.png", "dog-cardH.png", "dog-cardI.png"];
+var cardArray = [
+    "dog-cardA.png",
+    "dog-cardB.png",
+    "dog-cardC.png",
+    "dog-cardD.png",
+    "dog-cardE.png",
+    "dog-cardF.png",
+    "dog-cardG.png",
+    "dog-cardH.png",
+    "dog-cardI.png",
+    "dog-cardA.png",
+    "dog-cardB.png",
+    "dog-cardC.png", 
+    "dog-cardD.png",
+    "dog-cardE.png",
+    "dog-cardF.png",
+    "dog-cardG.png",
+    "dog-cardH.png",
+    "dog-cardI.png"
+];
 var randomizedOnce = [];
 
 //grabbed following code from https://bost.ocks.org/mike/shuffle/
@@ -157,3 +173,25 @@ function appendRandomizedCards() {
         $(targetedDiv).html('<img src=' + shuffledCards[i] + '>');
     }
 }
+
+//priorities--
+
+// more functionality:
+// dynamically create game board with jquery?
+
+// fix css and make it not so ugly
+// edit photos to at least give them a border so it looks better against the background of the page
+
+// potential things for later-
+// ask the owners of internet dogs i follow if i can use their dogs' photos - likely would only do this after the first set is finished and the css is fixed, so there is something to show them when i approach them.
+// perhaps when a match involving that dog is completed, there is a link offered to information about the breed and any related social media fan pages the owner may have for their dog
+// if this falls through - maybe try to find other professional stock photos with similar backgrounds - may look more professional if the backdrops are similar, would also make the dogs themselves stand out more
+// maybe play random dog-related sound effect when a match is made
+
+// known bugs to fix:
+// 1 when you click the same image twice, it sees the sources are the same and considers them a match
+// 2 if you click an unmatched card and then click a previously matched card, it sees they do not match, and flips both the unmatched AND the previously matched cards over.
+    // possible solution: put all the playable cards into an array, remove them from the array of clickable elements once it is matched?
+
+
+
