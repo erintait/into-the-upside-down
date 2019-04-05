ed3f6f6f72a140495d9194c623b26df5ccf05553
Erin Tait <erinmtait@gmail.com>
2019-04-05 13:02:02 -0700

    changed image sizes and quality

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 9;
 var match_counter = 0;
-var first_card_grandparent = null;
-var second_card_grandparent = null;
 var first_card_source='';
 var second_card_source='';
 var cant_click_card = false;
@@ -20,24 +18,25 @@ var games_played = 0;
 
 // cards
 cardArray = [
-    "images/card01.png",
-    "images/card02.png",
-    "images/card03.png",
-    "images/card04.png",
-    "images/card05.png",
-    "images/card07.png",
-    "images/card08.png",
-    "images/card09.png",
-    "images/card10.png",
-    "images/card01.png",
-    "images/card02.png",
-    "images/card03.png",
-    "images/card04.png",
-    "images/card05.png",
-    "images/card07.png",
-    "images/card08.png",
-    "images/card09.png",
-    "images/card10.png"
+    "images/card1.jpg",
+    "images/card2.jpg",
+    "images/card3.jpg",
+    "images/card4.jpg",
+    "images/card5.jpg",
+    "images/card6.jpg",
+    "images/card7.jpg",
+    "images/card8.jpg",
+    "images/card9.jpg",
+    "images/card1.jpg",
+    "images/card2.jpg",
+    "images/card3.jpg",
+    "images/card4.jpg",
+    "images/card5.jpg",
+    "images/card6.jpg",
+    "images/card7.jpg",
+    "images/card8.jpg",
+    "images/card9.jpg"
+    // "images/card10.jpg"
 ];
 var randomizedOnce = [];
 
@@ -46,12 +45,13 @@ function initializeApp(){
     addClickToReset();
     addClickToClose();
     appendRandomizedCards(cardArray);
-    displayRandomBG();
+    var bgArray = ['background1.jpg', 'background2.jpg', 'background3.jpg'];
+    $('#game-area').css({'background-image': 'url(images/' + bgArray[Math.floor(Math.random() * bgArray.length)] + ')'});
+    // displayRandomBG();
 }
 
 function displayRandomBG(){
-    var bgArray = ['background1.jpg', 'background2.jpg', 'background3.jpg'];
-    $('#game-area').css({'background-image': 'url(images/' + bgArray[Math.floor(Math.random() * bgArray.length)] + ')'});
+    
 }
 
 function displayStats(){
@@ -89,24 +89,25 @@ function reset_stats(){
     }
     $('.accuracy .value').text(accuracy);
     cardArray = [
-        "images/card01.png",
-        "images/card02.png",
-        "images/card03.png",
-        "images/card04.png",
-        "images/card05.png",
-        "images/card07.png",
-        "images/card08.png",
-        "images/card09.png",
-        "images/card10.png",
-        "images/card01.png",
-        "images/card02.png",
-        "images/card03.png",
-        "images/card04.png",
-        "images/card05.png",
-        "images/card07.png",
-        "images/card08.png",
-        "images/card09.png",
-        "images/card10.png"
+        "images/card1.jpg",
+        "images/card2.jpg",
+        "images/card3.jpg",
+        "images/card4.jpg",
+        "images/card5.jpg",
+        "images/card6.jpg",
+        "images/card7.jpg",
+        "images/card8.jpg",
+        "images/card9.jpg",
+        "images/card1.jpg",
+        "images/card2.jpg",
+        "images/card3.jpg",
+        "images/card4.jpg",
+        "images/card5.jpg",
+        "images/card6.jpg",
+        "images/card7.jpg",
+        "images/card8.jpg",
+        "images/card9.jpg"
+        // "images/card10.jpg"
     ];
     $('.card').removeClass('hidden');
     $('.card').removeClass('fade');
@@ -129,7 +130,7 @@ function hideMismatchedCards(){
 }
 
 function addClickToCards(){
-    $('.back > img').click(handleCardClick);
+    $('.card').click(handleCardClick);
 }
 
 function handleCardClick(){
@@ -176,68 +177,6 @@ function handleCardClick(){
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-    // if(cant_click_card){
-    //     return;
-    // }
-    // var card_clicked = event.currentTarget;
-    // if(first_card_clicked === null) {
-    //     first_card_clicked = event.currentTarget;
-        // first_card_grandparent = $(first_card_clicked).parent().parent();
-        // first_card_source = $(first_card_grandparent).find('.front > img').attr('src');
-        // console.log('first card source', first_card_source);
-        // first_card_source = first_card_clicked.find('.front > img').attr('src');
-        // console.log('first card source', first_card_source);
-        // console.log('first card clicked', first_card_clicked);
-//         first_card_source = $(event.currentTarget).find('.front > img').attr('src');
-//         revealCard(first_card_grandparent);
-//         $(first_card_clicked).off();
-//         return
-//     }
-//     else if(second_card_clicked === null) {
-//         second_card_clicked = event.currentTarget;
-//         second_card_grandparent = $(second_card_clicked).parent().parent();
-//         second_card_source = $(second_card_grandparent).find('front > img').attr('src');
-//         console.log('second card source', second_card_source);
-//         revealCard(second_card_grandparent);
-//         $(second_card_clicked).off();
-//         if(first_card_source === second_card_source) {
-//             debugger;
-//             matches++;
-//             match_counter++;
-//             attempts++;
-//             displayStats();
-//             $(first_card_clicked).addClass('fade');
-//             $(second_card_clicked).addClass('fade');
-//             first_card_clicked = null;
-//             second_card_clicked = null;
-//             if(match_counter === total_possible_matches) {
-//                 setTimeout(function(){
-//                     $('#modal').removeClass('modal-hide');
-//                 }, 2000);
-//             }
-//         }
-//         else {
-//             cant_click_card = true;
-//             $(first_card_clicked).on("click", handleCardClick);
-//             $(second_card_clicked).on("click", handleCardClick);
-//             attempts++;
-//             displayStats();
-//             setTimeout(hideMismatchedCards, 1500);
-//             cant_click_card = false;
-//             return
-//         }
-//     }
-// }
 
 window.onclick = function(event) {
     if (event.target == modal) {
